6410fb12478ed958b48204624a9beb42c8080a4f
bodyharvest217 <70770361+bodyharvest217@users.noreply.github.com>
2020-09-04 13:31:18 -0400

    Added label

--- Caffeine Overflow/WebForm1.aspx
+++ Caffeine Overflow/WebForm1.aspx
@@ -11,6 +11,7 @@
         <div>
             <asp:Label ID="Label1" runat="server" Text="Yo this is a test to see if you can see and edit this file with no issues"></asp:Label>
             <asp:Label ID="Label2" runat="server" Text="Add a label with your name "></asp:Label>
+            <asp:Label ID="Label3" runat="server" Text="Zeke was here."></asp:Label>
         <br />
         <br />
         Christian Guzman Was here...</form>
--- Caffeine Overflow/WebForm1.aspx.designer.cs
+++ Caffeine Overflow/WebForm1.aspx.designer.cs
@@ -40,5 +40,23 @@ namespace Caffeine_Overflow
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Label Label2;
+
+        /// <summary>
+        /// Label3 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label3;
+
+        /// <summary>
+        /// Label2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label2;
     }
 }
