f6b5ba8e93fad7df1348c43f5b2026903490c212
deshai89 <70532907+deshai89@users.noreply.github.com>
2020-10-24 11:56:59 -0500

    h

--- DnD Companion/DnD Companion/MonsterList.aspx.designer.cs
+++ DnD Companion/DnD Companion/MonsterList.aspx.designer.cs
@@ -7,11 +7,54 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
+<<<<<<< HEAD
 namespace DnD_Companion
 {
 
 
     public partial class MonsterList
     {
+=======
+namespace DnD_Companion {
+    
+    
+    public partial class MonsterList {
+        
+        /// <summary>
+        /// GridView1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView GridView1;
+        
+        /// <summary>
+        /// Table1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Table Table1;
+        
+        /// <summary>
+        /// Monster_list control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.SqlDataSource Monster_list;
+        
+        /// <summary>
+        /// Image1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Image Image1;
+>>>>>>> parent of 2546a2f... monser page updates
     }
 }
